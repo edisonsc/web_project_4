699dab44159e552e3ac2cbf607e3ddda9ba93076
Sarah Edison <edison.sarahc@gmail.com>
2021-08-25 21:07:27 -0400

    feat: add edit form functionality and load intial name and title values on modal open

--- script.js
+++ script.js
@@ -1,54 +1,43 @@
 //Form input query selectors
 let modal = document.querySelector(".popup");
-let name = document.querySelector(".form__input_name");
-let title = document.querySelector(".form__input_title");
+let nameInput = document.querySelector(".form__input_name");
+let jobInput = document.querySelector(".form__input_title");
 let saveButton = document.querySelector(".form__save-button");
+
 //profile information query selectors
 let profileName = document.querySelector(".profile__name");
 let profileTitle = document.querySelector(".profile__title");
+
 //edit button query selector
 let editButton = document.querySelector(".profile__edit-button");
+
+//edit button query selector
 let closeButton = document.querySelector(".popup__close-button")
 
+//form
+let formElement = document.querySelector(".form");// Use the querySelector() method
+
 //Functions
+//Adds intital values to modal and toggles open and close
 function toggleModal() {
-     modal.classList.toggle('popup_opened')
+  modal.classList.toggle('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileTitle.textContent; 
 };
 
 //EventListener
 editButton.addEventListener('click', toggleModal);
 closeButton.addEventListener('click', toggleModal);
 
+//form submit function
+function handleFormSubmit(evt) {
+  evt.preventDefault();
 
-// Let's find the form in the DOM
-let formElement = document.querySelector(".form");// Use the querySelector() method
+  profileName.textContent = nameInput.value;
+  profileTitle.textContent = jobInput.value;
+}
 
-  // Next is the form submit handler, though
-  // it won't submit anywhere just yet
-  function handleFormSubmit(evt) {
-    evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
-    // Having done so, we can define our own way of submitting the form.
-    // We'll explain it in more detail later.
-
-    // Let's find the form fields in the DOM
-    let nameInput = document.querySelector(".form__input_name"); // Use querySelector()
-    let jobInput = document.querySelector(".form__input_title");// Use querySelector()
-
-        // Get the values of each field from the corresponding value property
-       nameInput.value
-        // Select elements where the field values will be entered
-        let profileName = document.querySelector(".profile__name");
-
-        profileName.textContent= nameInput.nodeValue;
-        let profileTitle = document.querySelector(".profile__title");
-
-     
-        // Insert new values using the textContent property of the querySelector() method
-        let 
-  }
-
-// Connect the handler to the form:
-// it will watch the submit event
-formElement.addEventListener('submit', handleFormSubmit); 
+//form eventlistener
+formElement.addEventListener('submit', handleFormSubmit);
 
 handleFormSubmit();
