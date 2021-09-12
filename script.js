873ae9723703a967d9bdf7b7d22627c419493c26
Sarah Edison <edison.sarahc@gmail.com>
2021-09-12 15:15:11 -0400

    change let to const

--- script.js
+++ script.js
@@ -29,39 +29,38 @@ const initialCards = [
 //Select card template and content
 const cardTemplate = document.querySelector("#cardTemplate").content;
 const cardList = document.querySelector(".photo-grid");
-let cardImage = document.querySelector(".photo-grid__image");
-let cardTitle = document.querySelector(".photo-grid__title");
+const cardTitle = document.querySelector(".photo-grid__title");
 
 
 //Edit form query selectors
-let editModal = document.querySelector(".popup_type_edit");
-let editForm = editModal.querySelector(".form");
-let nameInput = document.querySelector(".form__input_type_name");
-let jobInput = document.querySelector(".form__input_type_title");
+const editModal = document.querySelector(".popup_type_edit");
+const editForm = editModal.querySelector(".form");
+const nameInput = document.querySelector(".form__input_type_name");
+const jobInput = document.querySelector(".form__input_type_title");
 
 //profile information query selectors
-let profileName = document.querySelector(".profile__name");
-let profileTitle = document.querySelector(".profile__title");
+const profileName = document.querySelector(".profile__name");
+const profileTitle = document.querySelector(".profile__title");
 
 //Add form query selectors
-let addModal = document.querySelector(".popup_type_add");
-let addForm = addModal.querySelector(".form");
-let placeInput = document.querySelector(".form__input_type_place");
-let linkInput = document.querySelector(".form__input_type_link");
+const addModal = document.querySelector(".popup_type_add");
+const addForm = addModal.querySelector(".form");
+const placeInput = document.querySelector(".form__input_type_place");
+const linkInput = document.querySelector(".form__input_type_link");
 
 //Preview query selectors
-let previewModal = document.querySelector(".popup_type_preview");
-let previewImage = document.querySelector(".popup__image");
-let previewCaption = document.querySelector(".popup__caption");
-let previewModalClose = previewModal.querySelector(".popup__close-button");
+const previewModal = document.querySelector(".popup_type_preview");
+const previewImage = document.querySelector(".popup__image");
+const previewCaption = document.querySelector(".popup__caption");
+const previewModalClose = previewModal.querySelector(".popup__close-button");
 
 //button selectors
-let editButton = document.querySelector(".profile__edit-button");
-let editModalClose = editModal.querySelector(".popup__close-button");
-let saveButton = document.querySelector("#save-button");
-let addButton = document.querySelector(".profile__add-button");
-let addModalClose = addModal.querySelector(".popup__close-button");
-let createButton = document.querySelector("#create-button");
+const editButton = document.querySelector(".profile__edit-button");
+const editModalClose = editModal.querySelector(".popup__close-button");
+const saveButton = document.querySelector("#save-button");
+const addButton = document.querySelector(".profile__add-button");
+const addModalClose = addModal.querySelector(".popup__close-button");
+const createButton = document.querySelector("#create-button");
 
 //Toggles all modals open and close
 function toggleModal(modalWindow) {
@@ -96,7 +95,9 @@ saveButton.addEventListener('click', () => toggleModal(editModal));
 //Image preview
 function handleImagePreview(card){
   previewImage.src = card.link;
+  previewImage.alt = card.name;
   previewCaption.textContent = card.name;
+
   toggleModal(previewModal);
   }
   
@@ -107,19 +108,21 @@ function handleImagePreview(card){
 function createNewCard(card){
   let cardElement = cardTemplate.querySelector(".photo-grid__card").cloneNode(true);
   //set card image
-  let cardImage = cardElement.querySelector(".photo-grid__image");
+  const cardImage = cardElement.querySelector(".photo-grid__image");
   cardImage.src = card.link;
+  //set card alt text
+  cardImage.alt = card.name;
   //set card title
   cardElement.querySelector(".photo-grid__title").textContent = card.name;
   //set delete icon
-  let deleteButton = cardElement.querySelector(".photo-grid__delete-icon");
+  const deleteButton = cardElement.querySelector(".photo-grid__delete-icon");
   deleteButton.addEventListener('click', handleRemoveCard);
   //set like button
-  let likeButton = cardElement.querySelector(".photo-grid__heart-icon");
+  const likeButton = cardElement.querySelector(".photo-grid__heart-icon");
   likeButton.addEventListener('click', handleLikeCard);
   //set image
   cardImage.addEventListener('click', () => handleImagePreview(card));
- //append to list
+ //append to lists
   return cardElement;
 };
 
@@ -137,7 +140,7 @@ function handleRemoveCard(evt){
 //Place form submit function
 function handlePlaceSubmit(evt) {
   evt.preventDefault();
-  let card = {
+  const card = {
     name: placeInput.value,
     link: linkInput.value,
   }
