d3516ed206a30642d6a83a9953e04687c7466774
Sarah Edison <edison.sarahc@gmail.com>
2021-09-13 19:20:21 -0400

    style: format script.js

--- script.js
+++ script.js
@@ -2,35 +2,34 @@
 const initialCards = [
   {
     name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
   },
   {
     name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
   },
   {
     name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
   },
   {
     name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
   },
   {
     name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
   },
   {
     name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
 ];
 
 //Select card template and content
 const cardTemplate = document.querySelector("#cardTemplate").content;
 const cardList = document.querySelector(".photo-grid");
 const cardTitle = document.querySelector(".photo-grid__title");
-
 
 //Edit form query selectors
 const editModal = document.querySelector(".popup_type_edit");
@@ -64,13 +63,13 @@ const createButton = document.querySelector("#create-button");
 
 //Toggles all modals open and close
 function toggleModal(modalWindow) {
-  modalWindow.classList.toggle('popup_opened');
- };
+  modalWindow.classList.toggle("popup_opened");
+}
 
-//Edit Form Profile 
+//Edit Form Profile
 //Adds initial values of name and job title to profile edit modal
-function prefillEditForm(){
-  if (editModal.classList.contains('popup')){
+function prefillEditForm() {
+  if (editModal.classList.contains("popup")) {
     nameInput.value = profileName.textContent;
     jobInput.value = profileTitle.textContent;
   }
@@ -83,30 +82,32 @@ function editFormSubmit(evt) {
   profileTitle.textContent = jobInput.value;
 }
 //Event listeners for edit form
-editForm.addEventListener('submit', editFormSubmit);
+editForm.addEventListener("submit", editFormSubmit);
 
-editButton.addEventListener('click', () => {
+editButton.addEventListener("click", () => {
   prefillEditForm();
   toggleModal(editModal);
 });
-editModalClose.addEventListener('click', () => toggleModal(editModal));
-saveButton.addEventListener('click', () => toggleModal(editModal));
+editModalClose.addEventListener("click", () => toggleModal(editModal));
+saveButton.addEventListener("click", () => toggleModal(editModal));
 
 //Image preview
-function handleImagePreview(card){
+function handleImagePreview(card) {
   previewImage.src = card.link;
   previewImage.alt = card.name;
   previewCaption.textContent = card.name;
 
   toggleModal(previewModal);
-  }
-  
-  //preview modal event listeners
-  previewModalClose.addEventListener('click', () => toggleModal(previewModal));
+}
+
+//preview modal event listeners
+previewModalClose.addEventListener("click", () => toggleModal(previewModal));
 
 //Create new card
-function createNewCard(card){
-  let cardElement = cardTemplate.querySelector(".photo-grid__card").cloneNode(true);
+function createNewCard(card) {
+  let cardElement = cardTemplate
+    .querySelector(".photo-grid__card")
+    .cloneNode(true);
   //set card image
   const cardImage = cardElement.querySelector(".photo-grid__image");
   cardImage.src = card.link;
@@ -116,52 +117,52 @@ function createNewCard(card){
   cardElement.querySelector(".photo-grid__title").textContent = card.name;
   //set delete icon
   const deleteButton = cardElement.querySelector(".photo-grid__delete-icon");
-  deleteButton.addEventListener('click', handleRemoveCard);
+  deleteButton.addEventListener("click", handleRemoveCard);
   //set like button
   const likeButton = cardElement.querySelector(".photo-grid__heart-icon");
-  likeButton.addEventListener('click', handleLikeCard);
+  likeButton.addEventListener("click", handleLikeCard);
   //set image
-  cardImage.addEventListener('click', () => handleImagePreview(card));
- //append to lists
+  cardImage.addEventListener("click", () => handleImagePreview(card));
+  //append to lists
   return cardElement;
-};
+}
 
 //Like a card
-function handleLikeCard(evt){
-  evt.target.classList.toggle('photo-grid__heart-icon_active');
+function handleLikeCard(evt) {
+  evt.target.classList.toggle("photo-grid__heart-icon_active");
 }
 //Remove a card
-function handleRemoveCard(evt){ 
-    const item = evt.target.closest(".photo-grid__card");
-    item.remove()
-  };
+function handleRemoveCard(evt) {
+  const item = evt.target.closest(".photo-grid__card");
+  item.remove();
+}
 
-//Add a new place to cards 
+//Add a new place to cards
 //Place form submit function
 function handlePlaceSubmit(evt) {
   evt.preventDefault();
   let card = {
     name: placeInput.value,
     link: linkInput.value,
-  }
+  };
   let cardElement = createNewCard(card);
   cardList.prepend(cardElement);
   placeInput.value = "";
   linkInput.value = "";
- }
+}
 
 //Add form EventListener
-addForm.addEventListener('submit', handlePlaceSubmit);
-addButton.addEventListener('click', () => {
+addForm.addEventListener("submit", handlePlaceSubmit);
+addButton.addEventListener("click", () => {
   toggleModal(addModal);
 });
-addModalClose.addEventListener('click', () => toggleModal(addModal));
-createButton.addEventListener('click', () => toggleModal(addModal));
+addModalClose.addEventListener("click", () => toggleModal(addModal));
+createButton.addEventListener("click", () => toggleModal(addModal));
 
 //Actions
 //Populate page with cards from initialCards array
 initialCards.forEach((card) => {
   cardElement = createNewCard(card);
   //append to list
-  cardList.append(cardElement)
+  cardList.append(cardElement);
 });
