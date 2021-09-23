6142e142c8e5b63c967d1645d9931b696a032b58
Sarah Edison <edison.sarahc@gmail.com>
2021-09-22 22:16:20 -0400

    feat: add function to show and hide error messages and toggle the button state when valid

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,28 +1,48 @@
+const isValid = (inputElement) => {
+    return inputElement.validity.valid;
+}
+
 const checkInputValidity = (formElement, inputElement, settings) => {
     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
 
-    if(!inputElement.validity.valid){
-    //show the error message and add the error class
-    inputElement.classList.add(settings.inputErrorClass);
-    errorElement.innerText = "Please fill out this field"
+    if (!isValid(inputElement)) {
+        //show the error message and add the error class
+        inputElement.classList.add(settings.inputErrorClass);
+        errorElement.innerText = inputElement.validationMessage;
+        errorElement.classList.add(settings.errorClass);
 
-} else {
-    //hide the error
-    inputElement.classList.remove(settings.inputErrorClass)
+    } else {
+        //hide the error
+        inputElement.classList.remove(settings.inputErrorClass);
+        errorElement.innerText = "";
+        errorElement.classList.remove(settings.errorClass);
+    };
 }
-};
+
+toggleButtonState = (inputList, button, settings) => {
+   const allValid = inputList.every(inputElement => isValid(inputElement));
+   if (allValid){
+       button.classList.remove(settings.inactiveButtonClass);
+       button.disabled = false;
+   } else {
+       button.classList.add(settings.inactiveButtonClass);
+       button.disabled = true;
+   }
+}
 
 const setupEventListeners = (formElement, settings) => {
     //get form elements
- const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
- 
- //setup listeners for the form elements
- inputList.forEach(inputElement => {
-     inputElement.addEventListener('change', (evt) => {
-         //check if input is valid
-         checkInputValidity(formElement, inputElement, settings)  
-     })
- })
+    const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
+    const button = formElement.querySelector(settings.submitButtonSelector);
+
+    //setup listeners for the form elements
+    inputList.forEach(inputElement => {
+        inputElement.addEventListener('input', (evt) => {
+            //check if input is valid
+            checkInputValidity(formElement, inputElement, settings);
+            toggleButtonState(inputList, button, settings)
+        })
+    })
 }
 
 // enabling validation by calling enableValidation()
@@ -36,7 +56,7 @@ const enableValidation = (settings) => {
         formElement.addEventListener('submit', (evt) => {
             evt.preventDefault();
         });
-    //setup event listeners for the form fields
+        //setup event listeners for the form fields
         setupEventListeners(formElement, settings);
     })
 };
