3fd93f4d9da59fd6575104e56d8411a301880e6d
Sarah Edison <edison.sarahc@gmail.com>
2021-10-13 21:34:01 -0400

    feat: set up event listeners and handlers in card class, import close/open modal functions

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,3 +1,39 @@
+//Preview query selectors
+const previewModal = document.querySelector(".popup_type_preview");
+const previewImage = document.querySelector(".popup__image");
+const previewCaption = document.querySelector(".popup__caption");
+const previewModalClose = previewModal.querySelector(".popup__close-button");
+
+//Opens modal 
+function openModal(modalWindow) {
+    modalWindow.classList.add("popup_opened");
+    document.addEventListener('keydown', escapeModal);
+    document.addEventListener('click', closeOverlay);
+  };
+  
+  //Closes modal
+  function closeModal(modalWindow) {
+    modalWindow.classList.remove("popup_opened");
+    document.removeEventListener('keydown', escapeModal);
+    document.removeEventListener('click', closeOverlay);
+  };
+  
+  //closes modal on escape
+  function escapeModal(evt) {
+    const modalWindow = document.querySelector(".popup_opened")
+    if (evt.key === "Escape") {
+      closeModal(modalWindow)
+    }
+  };
+  
+  //closes modal on overlay click
+function closeOverlay(evt) {
+    const modalWindow = document.querySelector(".popup_opened")
+    if (evt.target === modalWindow) {
+      closeModal(modalWindow)
+    }
+  };
+
 class Card {
     constructor(data, cardSelector) {
         this._name = data.name;
@@ -8,50 +44,49 @@ class Card {
 
     _getTemplate() {
         const cardElement = document
-        .querySelector(this._cardSelector)
-        .content.querySelector('.photo-grid-card')
-        .cloneNode(true);
+            .querySelector(this._cardSelector)
+            .content.querySelector('.photo-grid__card')
+            .cloneNode(true);
 
         return cardElement
     }
 
+    _handleLikeCard() {
+        this._element.querySelector('.photo-grid__heart-icon').classList.toggle('photo-grid__heart-icon_active')
+    }
+
+    _handleRemoveCard() {
+        this._element.remove();
+    }
+
+    _handlePreviewImage() {
+        previewImage.src = this._link;
+        previewImage.alt = this._name;
+        previewCaption.textContent = this._name;
+
+        openModal(previewModal)
+    }
+
     _setEventListeners() {
         //set up events
-        console.warn('setEvent Listeners is not complete')
+        console.warn('setEvent Listeners is not complete');
+        this._element.querySelector('.photo-grid__heart-icon').addEventListener("click", () => { this._handleLikeCard() });
+        this._element.querySelector('.photo-grid__delete-icon').addEventListener("click", () => { this._handleRemoveCard() });
+        this._element.querySelector('.photo-grid__image').addEventListener("click", () => {this._handlePreviewImage() });
+
     }
 
     generateCard() {
-        this._card = this._getTemplate();
+        this._element = this._getTemplate();
+
         this._setEventListeners();
 
-        const cardImage = this._card.querySelector('.photo-grid__image');
-        cardImage
-        this._card.querySelector('.photo-grid__title').textContent = this._name
+        this._element.querySelector('.photo-grid__image').src = this._link;
+
+        this._element.querySelector('.photo-grid__title').textContent = this._name;
+
+        return this._element
     }
 }
 
 export default Card
-
-//Create new card
-function createNewCard(card) {
-    const cardElement = cardTemplate
-        .querySelector(".photo-grid__card")
-        .cloneNode(true);
-    //set card image
-    const cardImage = cardElement.querySelector(".photo-grid__image");
-    cardImage.src = card.link;
-    //set card alt text
-    cardImage.alt = card.name;
-    //set card title
-    cardElement.querySelector(".photo-grid__title").textContent = card.name;
-    //set delete icon
-    const deleteButton = cardElement.querySelector(".photo-grid__delete-icon");
-    deleteButton.addEventListener("click", handleRemoveCard);
-    //set like button
-    const likeButton = cardElement.querySelector(".photo-grid__heart-icon");
-    likeButton.addEventListener("click", handleLikeCard);
-    //set image
-    cardImage.addEventListener("click", () => handleImagePreview(card));
-    //append to lists
-    return cardElement;
-}
