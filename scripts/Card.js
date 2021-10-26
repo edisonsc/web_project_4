3e9e90697cdc85b0bcfb16dd38f30a9d3f822d70
Sarah Edison <edison.sarahc@gmail.com>
2021-10-26 18:55:04 -0400

    merge Card

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
 import {previewModal, previewImage, previewCaption, openModal} from "./utils.js"
-=======
-//Preview query selectors
-const previewModal = document.querySelector(".popup_type_preview");
-const previewImage = document.querySelector(".popup__image");
-const previewCaption = document.querySelector(".popup__caption");
-const previewModalClose = previewModal.querySelector(".popup__close-button");
-
-//Opens modal 
-function openModal(modalWindow) {
-    modalWindow.classList.add("popup_opened");
-    document.addEventListener('keydown', escapeModal);
-    document.addEventListener('click', closeOverlay);
-  };
-  
-  //Closes modal
-  function closeModal(modalWindow) {
-    modalWindow.classList.remove("popup_opened");
-    document.removeEventListener('keydown', escapeModal);
-    document.removeEventListener('click', closeOverlay);
-  };
-  
-  //closes modal on escape
-  function escapeModal(evt) {
-    const modalWindow = document.querySelector(".popup_opened")
-    if (evt.key === "Escape") {
-      closeModal(modalWindow)
-    }
-  };
-  
-  //closes modal on overlay click
-function closeOverlay(evt) {
-    const modalWindow = document.querySelector(".popup_opened")
-    if (evt.target === modalWindow) {
-      closeModal(modalWindow)
-    }
-  };
->>>>>>> feature/Card
 
 class Card {
     constructor(data, cardSelector) {
