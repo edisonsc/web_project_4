064e94da0523bf8010f29cdfeab8d9d545779963
Sarah Edison <edison.sarahc@gmail.com>
2021-10-26 18:41:35 -0400

    feat: add Section class

---
+++ scripts/Section.js
@@ -0,0 +1,10 @@
+// Look at Lesson 5 Relationships between classes
+class Section {
+    constructor({items, renderer}, cardSelector){
+       this._items = items,
+       this.
+    }
+
+    renderer(){}
+    addItem(){}
+}
