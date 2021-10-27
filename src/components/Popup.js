0108192a5b2bd4ade562b15aadab20339f3c1a07
Sarah Edison <edison.sarahc@gmail.com>
2021-10-26 20:10:46 -0400

    feat: add Popup functions

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,6 +1,33 @@
 class Popup {
     constructor(popupSelector) {
-        this._popupElement = document.querySelector(popupSelector);
+        this._popupSelector = popupSelector;
+    }
 
+    open(){
+        this._popupSelector.classList.add("popup_opened");
+    }
+
+    close(){
+        this._popupSelector.classList.remove("popup_opened");
+    }
+
+    _handleEscClose(evt){  
+            this._popupSelector = document.querySelector(".popup_opened")
+            if (evt.key === "Escape") {
+                this.close(this._popupSelector)
+            }
+        };
+    
+    _handleCloseOverlay(evt){
+            this._popupSelector = document.querySelector(".popup_opened")
+            if (evt.target === this._popupSelector) {
+                this.close(this._popupSelector)
+            }
+        };
+
+    setEventListeners(){
+        this._popupSelector.querySelector(".popup__close-button").addEventListener("click", () => {this.close()})
     }
 }
+
+export default Popup
