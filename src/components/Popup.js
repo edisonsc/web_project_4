037d711e422e989d69e8f34a3e49a48c1ac41b8e
Sarah Edison <edison.sarahc@gmail.com>
2021-10-27 21:06:01 -0400

    feat: Popup add _handleCloseOverlay and bind to constructor

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,33 +1,46 @@
 class Popup {
     constructor(popupSelector) {
-        this._popupSelector = popupSelector;
+        this._popupElement = popupSelector;
+        this._handleEscClose = this._handleEscClose.bind(this);
+        this._handleCloseOverlay = this._handleCloseOverlay.bind(this);
     }
 
     open(){
-        this._popupSelector.classList.add("popup_opened");
+        this._popupElement.classList.add("popup_opened");
+        document.addEventListener('keydown', this._handleEscClose);
+        document.addEventListener('click', this._handleCloseOverlay);
     }
 
     close(){
-        this._popupSelector.classList.remove("popup_opened");
+        this._popupElement.classList.remove("popup_opened");
+        document.removeEventListener('keydown', this._handleEscClose);
+        document.removeEventListener('click', this._handleCloseOverlay);
     }
 
     _handleEscClose(evt){  
-            this._popupSelector = document.querySelector(".popup_opened")
+            this._popupElement = document.querySelector(".popup_opened")
             if (evt.key === "Escape") {
                 this.close(this._popupSelector)
             }
         };
     
     _handleCloseOverlay(evt){
-            this._popupSelector = document.querySelector(".popup_opened")
+            this._popupElement = document.querySelector(".popup_opened")
             if (evt.target === this._popupSelector) {
                 this.close(this._popupSelector)
             }
         };
 
     setEventListeners(){
-        this._popupSelector.querySelector(".popup__close-button").addEventListener("click", () => {this.close()})
+        this._popupElement.querySelector(".popup__close-button").addEventListener("click", () => {this.close()})
     }
 }
 
 export default Popup
+
+// handleEscapeUp(evt){
+//     evt.preventDefault();
+//     if(evt.which === ESC_KEYCODE) {
+//         this.close();
+//     }
+// }
