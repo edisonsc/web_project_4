dfaa4c717ad442ddd3cfd1e698ace3aa2ab321d3
Sarah Edison <edison.sarahc@gmail.com>
2021-10-28 18:40:16 -0400

    feat: popupwithform class

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,26 @@
+import Popup from './Popup';
+
+class PopupWithForm extends Popup {
+    constructor(){
+
+    }
+  _getInputValues(){
+
+  }
+
+  setEventListeners(){
+    this._popupElement.querySelector(".popup__close-button").addEventListener("click", () => {this.close()});
+    this._popupElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._popupElement.reset();
+    })
+  } 
+  
+
+  close(){
+
+    super.close()
+  }
+}
+
+export default PopupWithForm
