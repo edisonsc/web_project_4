c077f9c820c80116d033e4b90880263a34b12a6e
Sarah Edison <edison.sarahc@gmail.com>
2021-10-28 19:37:45 -0400

    feat:add UserInfo class

---
+++ src/components/UserInfo.js
@@ -0,0 +1,15 @@
+class UserInfo {
+    constructor(name, title) {
+        this.name = name,
+        this.title = title
+    }
+    getUserInfo(){
+
+    }
+
+    setUserInfo(){
+        
+    }
+}
+
+export default UserInfo
