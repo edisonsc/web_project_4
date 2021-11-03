9d32d39bcdf814abd11dfc8f1fdea5545b6f90da
Sarah Edison <edison.sarahc@gmail.com>
2021-11-03 18:38:05 -0400

    outline UserInfo class

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,15 +1,20 @@
 class UserInfo {
-    constructor(name, title) {
-        this.name = name,
-        this.title = title
+    constructor(nameSelector, titleSelector) {
+        this._nameElement = document.querySelector(nameSelector)
+        this._titleElement = document.querySelector(titleSelector)
     }
     getUserInfo(){
-
+            //returns an object of this._name and this._title
+            return {name: this._nameElement.textContent, title: this._titleElement.textContent}
     }
 
-    setUserInfo(){
-        
+    setUserInfo(name, title){
+        //takes a name and title
+        this._nameElement.textContent = name
     }
 }
 
 export default UserInfo
+
+//constructor(popupSelector) {
+    this._popupElement = document.querySelector(popupSelector);
