376775096f11795d10150a361e4c5640afe73806
Sarah Edison <edison.sarahc@gmail.com>
2021-11-03 18:37:56 -0400

    style: correct popup__caption querySelector

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,8 +3,8 @@ import Popup from './Popup.js';
 class PopupWithImage extends Popup {
    
     open({link, name}){
-        this._popupElement.querySelector('.popup_caption').textContent = name
-        const image =  this._popupElement.querySelector('.popup_image')
+        this._popupElement.querySelector('.popup__caption').textContent = name
+        const image =  this._popupElement.querySelector('.popup__image')
         image.src = link;
         image.alt = name;
         super.open();
