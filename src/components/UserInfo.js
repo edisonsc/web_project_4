f80d897f64ee1c28e6858d1be9b872d1f83bfd59
Sarah Edison <edison.sarahc@gmail.com>
2021-11-06 12:42:26 -0400

    style: remove comments

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -4,12 +4,10 @@ class UserInfo {
         this._titleElement = document.querySelector(titleSelector)
     }
     getUserInfo(){
-            //returns an object of this._name and this._title
             return {name: this._nameElement.textContent, title: this._titleElement.textContent}
     }
 
     setUserInfo(name, title){
-        //takes a name and title
         this._nameElement.textContent = name
         this._titleElement.textContent = title
     }
