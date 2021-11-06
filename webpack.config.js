20529497337402a7ad7541ee988ec213c31b5ca8
Sarah Edison <edison.sarahc@gmail.com>
2021-11-06 17:53:20 -0400

    remove port from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,53 +5,53 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    devtool: 'inline-source-map',
-    entry: {
-        main: "./src/pages/index.js",
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-        publicPath: "",
-        clean: true,
-    },
-    target: ['web', 'es5'],
-    stats: { children: true },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: "/node_modules/"
-              },
-              {
-                test: /\.css$/,
-                use: [
-                  MiniCssExtractPlugin.loader,
-                  {
-                    loader: "css-loader",
-                    options: {importLoaders: 1}
-                  },
-                  "postcss-loader"
-                ]
-              },
-              {
-                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-                type: "asset/resource"
-              },
+  entry: {
+    main: "./src/pages/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+    clean: true,
+  },
+  target: ['web', 'es5'],
+  stats: { children: true },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 }
+          },
+          "postcss-loader"
         ]
-    },
-
-    devServer: {
-        static: './dist',
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource"
+      },
     ]
+  },
+
+  devServer: {
+    static: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ]
 }
