035cf76437952c45a7b3199edfee0bfad56874d3
Sarah Edison <edison.sarahc@gmail.com>
2021-11-06 12:42:10 -0400

    feat: reset form and add setDefaultValues to PoupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,21 +1,35 @@
 import Popup from './Popup.js';
 
 class PopupWithForm extends Popup {
-  constructor(popupSelector, handleFormSubmit) {
+  constructor({ popupSelector, handleFormSubmit }) {
     super(popupSelector);
     this._handleFormSubmit = handleFormSubmit;
   }
 
-  _setEventListeners() {
-    this._popupElement.addEventListener("submit", (evt) => {
+  setEventListeners() {
+    this._popupElement.querySelector(".form").addEventListener("submit", (evt) => {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
-      this._popupElement.close();
-      this._popupElement.reset();
+      this.close()
     })
+    this._popupElement.querySelector(".popup__close-button").addEventListener("click", this.close);
+    super.setEventListeners();
   }
+
+  close() {
+    this._popupElement.querySelector(".form").reset()
+    super.close();
+  }
+
+  setDefaultValues(user){
+    this._popupElement.querySelector(".form__input_type_name").value = user['name']
+    this._popupElement.querySelector(".form__input_type_title").value = user['title']
+
+  }
+
+
   _getInputValues() {
-    this._inputList = this._element.querySelectorAll(".form__input")
+    this._inputList = this._popupElement.querySelectorAll(".form__input")
     this._formValues = {};
     this._inputList.forEach((input) => {
       this._formValues[input.name] = input.value;
