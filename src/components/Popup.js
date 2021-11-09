18bd09ceeeb329567eb0ad2e55daaaf028a7045f
Sarah Edison <edison.sarahc@gmail.com>
2021-11-09 18:09:01 -0500

    remove handleOverlay

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,20 +2,18 @@ class Popup {
     constructor(popupSelector) {
         this._popupElement = document.querySelector(popupSelector);
         this._handleEscClose = this._handleEscClose.bind(this);
-        this._handleCloseOverlay = this._handleCloseOverlay.bind(this);
         this.close = this.close.bind(this)
     }
 
     open() {
         this._popupElement.classList.add("popup_opened");
         document.addEventListener('keydown', this._handleEscClose);
-        document.addEventListener('click', this._handleCloseOverlay);
     }
 
     close() {
         this._popupElement.classList.remove("popup_opened");
         document.removeEventListener('keydown', this._handleEscClose);
-        document.removeEventListener('click', this._handleCloseOverlay);
+
     }
 
     _handleEscClose(evt) {
