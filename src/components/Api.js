52f7c59357816606a1f44154ad5c8ff0de468522
Sarah Edison <edison.sarahc@gmail.com>
2021-11-10 15:41:21 -0500

    feat: refactor class Api to break handle response and error functions

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,34 +1,73 @@
 class Api {
-    constructor({baseUrl, headers}){
-        this._baseUrl = baseUrl,
-        this._headers = headers  
-    }
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl,
+      this._headers = headers,
+      this._handleResponse = this._handleResponse.bind(this),
+      this._handleErrorResponse = this._handleErrorResponse.bind(this)
+  }
 
-    getInitialCards() {
-       return fetch(this._baseUrl, {
-          headers: this._headers,
-        })
-          .then(res => {
-            if (res.ok) {
-              return res.json();
-            }
-            // if the server returns an error, reject the promise
-            return Promise.reject(`Error: ${res.status}`);
-          })  
+  _handleResponse(res) {
+    if (res.ok) {
+      return res.json()
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  _handleErrorResponse(err) {
+    console.log("this is an error", err)
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}cards`, {
+      headers: this._headers,
+    })
+      .then(this._handleResponse)
+      .catch(this._handleErrorResponse)
+  }
+
+  addCard(data) {
+    return fetch(this_baseUrl, {
+      headers: this._headers,
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then(this._handleResponse)
+      .catch(this._handleErrorResponse)
+  }
+
+
+  deleteCard(_id) {
+    return fetch(`${this._baseUrl}${_id}`, {
+      headers: this._headers,
+      method: "DELETE",
+      body: JSON.stringify(data),
+    })
+    .then(this._handleResponse)
+    .catch(this._handleErrorResponse)
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}users/me`, {
+      headers: this._headers,
+      method: "GET",
+    })
+      .then(this._handleResponse)
+      .catch(this._handleErrorResponse)
+  }
+
+  editUserInfo() {
+    return fetch(`${this._baseUrl}users/me`, {
+      headers: this._headers,
+      method: "PATCH",
+      body: JSON.stringify({
+        name: "me",
+        about: "title"
+      })
+    })
+      .then(this._handleResponse)
+      .catch(this._handleErrorResponse)
+  }
+
 }
 
-    addCard(data) {
-      return fetch(this_baseUrl, {
-        headers: this._headers,
-        method: "POST",
-        body: JSON.stringify(data),
-      })
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(`Error: ${res.status}`);
-      })
-    }
-}
 export default Api
