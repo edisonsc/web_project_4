158fb6ca880caf6821794737672611579e549efe
Sarah Edison <edison.sarahc@gmail.com>
2021-11-10 20:10:56 -0500

    feat: add avatarElement to class UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,15 +1,17 @@
 class UserInfo {
-    constructor(nameSelector, titleSelector) {
+    constructor(nameSelector, titleSelector, avatarSelector) {
         this._nameElement = document.querySelector(nameSelector)
         this._titleElement = document.querySelector(titleSelector)
+        this._avatarElement = document.querySelector(avatarSelector)
     }
     getUserInfo() {
         return { name: this._nameElement.textContent, title: this._titleElement.textContent }
     }
 
-    setUserInfo(name, title) {
+    setUserInfo(name, about, avatar) {
         this._nameElement.textContent = name
-        this._titleElement.textContent = title
+        this._titleElement.textContent = about
+        this._avatarElement.src = avatar
     }
 }
 
