c6e4e936359c006d677bfaab1ce18c2c3b6a97f2
Sarah Edison <edison.sarahc@gmail.com>
2021-11-11 21:34:09 -0500

    feat: user info class

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,16 +1,13 @@
 class UserInfo {
-    constructor(nameSelector, titleSelector) {
-        this._nameElement = document.querySelector(nameSelector)
-        this._titleElement = document.querySelector(titleSelector)
+    constructor({nameSelector, titleSelector, avatarSelector}) {
+        this._nameElement = document.querySelector(nameSelector),
+        this._titleElement = document.querySelector(titleSelector),
+        this._avatarElement = document.querySelector(avatarSelector)
     }
-    getUserInfo() {
-        return { name: this._nameElement.textContent, title: this._titleElement.textContent }
-    }
-
-    setUserInfo(name, about, avatar) {
-        this._nameElement.textContent = name
-        this._titleElement.textContent = about
-        this._avatarElement.src = avatar
+    setUserInfo(res) {
+        this._nameElement.textContent = res.name
+        this._titleElement.textContent = res.about
+        this._avatarElement.src = res.avatar
     }
 }
 
