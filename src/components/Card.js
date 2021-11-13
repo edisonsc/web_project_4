0db201546f6c19056bdc704bf9cfbcbc33fd66e1
Sarah Edison <edison.sarahc@gmail.com>
2021-11-12 22:14:31 -0500

    feat: Card class create delete button in generateCard

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 class Card {
-    constructor({ data, handleCardClick, cardSelector }) {
+    constructor({ data, handleCardClick, handleLikeCard, handleRemoveCard, cardSelector }) {
         this._name = data.name;
         this._link = data.link;
         this._cardId = data._id;
         this._likes = data.likes;
+        this._cardOwner = data.owner._id;
         this._cardSelector = cardSelector;
         this._handleCardClick = handleCardClick;
+        this._handleLikeCard = handleLikeCard;
+        this._handleRemoveCard = handleRemoveCard
     }
 
     _getTemplate() {
@@ -21,22 +24,16 @@ class Card {
         this._element.querySelector('.photo-grid__heart-icon').classList.toggle('photo-grid__heart-icon_active')
     }
 
-    _handleRemoveCard() {
-    
-        this._element.remove();
-        this._element = null;
-
-    }
 
     _setEventListeners() {
         //set up events
         this._element.querySelector('.photo-grid__heart-icon').addEventListener("click", () => { this._handleLikeCard() });
-        // this._element.querySelector('.photo-grid__delete-icon').addEventListener("click", () => { this._handleRemoveCard() });
+        this._element.querySelector('.photo-grid__delete-icon').addEventListener("click", () => { this._handleRemoveCard({ id: this._cardId}) });
         this._element.querySelector('.photo-grid__image').addEventListener("click", () => { this._handleCardClick({ link: this._link, name: this._name }) });
 
     }
 
-    generateCard() {
+    generateCard(user) {
         this._element = this._getTemplate();
 
         this._setEventListeners();
@@ -48,9 +45,14 @@ class Card {
         this._element.querySelector('.photo-grid__title').textContent = this._name;
         
         this._element.querySelector('.photo-grid__text').textContent = this._likes.length;
+       
+        if (this._cardOwner !== user) {
+            this._element.querySelector('.photo-grid__delete-icon').hidden = true;
+        }
 
         return this._element
     }
+   
 }
 
 export default Card
