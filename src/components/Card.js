59053be250d17ab73f402e849ac2818c8be9c934
Sarah Edison <edison.sarahc@gmail.com>
2021-11-13 16:07:42 -0500

    feat: card like feature in generateCard

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ class Card {
         this._link = data.link;
         this._cardId = data._id;
         this._likes = data.likes;
+        this._cardLiked = false;
         this._cardOwner = data.owner._id;
         this._cardSelector = cardSelector;
         this._handleCardClick = handleCardClick;
@@ -20,17 +21,29 @@ class Card {
         return cardElement
     }
 
-    _handleLikeCard() {
-        this._element.querySelector('.photo-grid__heart-icon').classList.toggle('photo-grid__heart-icon_active')
-    }
-
-
     _setEventListeners() {
         //set up events
-        this._element.querySelector('.photo-grid__heart-icon').addEventListener("click", () => { this._handleLikeCard() });
-        this._element.querySelector('.photo-grid__delete-icon').addEventListener("click", () => { this._handleRemoveCard({ id: this._cardId}) });
-        this._element.querySelector('.photo-grid__image').addEventListener("click", () => { this._handleCardClick({ link: this._link, name: this._name }) });
+        this._element.querySelector('.photo-grid__heart-icon').addEventListener("click",
+            () => {
+                this._handleLikeCard( this );
+            })
 
+        this._element.querySelector('.photo-grid__delete-icon').addEventListener("click",
+            () => {
+                this._handleRemoveCard({ id: this._cardId });
+            })
+
+        this._element.querySelector('.photo-grid__image').addEventListener("click",
+            () => {
+                this._handleCardClick({ link: this._link, name: this._name });
+            })
+
+    };
+
+    updateCount(res){     
+        this._element.querySelector('.photo-grid__text').textContent = res.likes.length;
+         this._cardLiked = !this._cardLiked;
+         this._element.querySelector('.photo-grid__heart-icon').classList.toggle('photo-grid__heart-icon_active')       
     }
 
     generateCard(user) {
@@ -43,16 +56,25 @@ class Card {
         this._element.querySelector('.photo-grid__image').alt = this._name;
 
         this._element.querySelector('.photo-grid__title').textContent = this._name;
-        
+
         this._element.querySelector('.photo-grid__text').textContent = this._likes.length;
+
        
+        this._likes.forEach(like => {
+            if (like._id === user) {
+                this._cardLiked = true;
+                this._element.querySelector('.photo-grid__heart-icon').classList.toggle('photo-grid__heart-icon_active');
+            }
+        })
+
         if (this._cardOwner !== user) {
             this._element.querySelector('.photo-grid__delete-icon').hidden = true;
         }
 
+
         return this._element
     }
-   
+
 }
 
 export default Card
