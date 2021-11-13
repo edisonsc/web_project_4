a400611f4ecaf25e0552111cbcbe84f7f2cad717
Sarah Edison <edison.sarahc@gmail.com>
2021-11-13 16:08:20 -0500

    feat: work on removing card from section

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,6 +8,15 @@ class Section {
         this._container.prepend(element)
     }
 
+    removeItem(element_id){
+        this._items.forEach(element => {
+           
+            if (element_id === element._id){
+                console.log(element_id, element._id, element);
+               this._items.remove(element)
+            }
+        });
+    }
     renderItems() {
         this._items.forEach((item) => {
             this._renderer(item)
