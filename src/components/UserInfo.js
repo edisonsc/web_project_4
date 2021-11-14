2af562e53b1c6f820c953d7b44eca475abdc140f
Sarah Edison <edison.sarahc@gmail.com>
2021-11-14 09:29:11 -0500

    style: format getUserInfo in userInfo class

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,13 +1,18 @@
 class UserInfo {
-    constructor({nameSelector, titleSelector, avatarSelector}) {
+    constructor({ nameSelector, titleSelector, avatarSelector }) {
         this._nameElement = document.querySelector(nameSelector),
-        this._titleElement = document.querySelector(titleSelector),
-        this._avatarElement = document.querySelector(avatarSelector)
+            this._titleElement = document.querySelector(titleSelector),
+            this._avatarElement = document.querySelector(avatarSelector)
     }
 
-    getUserInfo(){
-        return {name: this._nameElement.textContent, about:this._titleElement.textContent, avatar: this._avatarElement.src  }
+    getUserInfo() {
+        return { 
+            name: this._nameElement.textContent, 
+            about: this._titleElement.textContent, 
+            avatar: this._avatarElement.src
+         }
     }
+
     setUserInfo(res) {
         this._nameElement.textContent = res.name
         this._titleElement.textContent = res.about
