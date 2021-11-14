225953e90e7900c9b9a748d893f5e227cdaceec4
Sarah Edison <edison.sarahc@gmail.com>
2021-11-14 09:26:45 -0500

    hotfix: move config to constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+
+//Api
+const config = {
+    baseUrl: "https://around.nomoreparties.co/v1/group-11/",
+    headers: {
+      authorization: "ce0cfad9-c022-44c4-8673-897f4eaf9eed",
+      "Content-Type": "application/json"
+    }
+  }
+
+  export {config}
