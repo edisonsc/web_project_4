dd65ef9e4d61bf48db140500fbe374771b330527
Sarah Edison <edison.sarahc@gmail.com>
2021-11-16 08:06:38 -0500

    style: reformat constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,10 @@
-
 //Api
 const config = {
-    baseUrl: "https://around.nomoreparties.co/v1/group-11/",
-    headers: {
-      authorization: "ce0cfad9-c022-44c4-8673-897f4eaf9eed",
-      "Content-Type": "application/json"
-    }
-  }
+  baseUrl: "https://around.nomoreparties.co/v1/group-11/",
+  headers: {
+    authorization: "ce0cfad9-c022-44c4-8673-897f4eaf9eed",
+    "Content-Type": "application/json",
+  },
+};
 
-  export {config}
+export { config };
