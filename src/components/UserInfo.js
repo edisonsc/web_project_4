ccfc7fcb12be4616459ba4a90beb10e27055fd79
Sarah Edison <edison.sarahc@gmail.com>
2021-11-16 08:07:51 -0500

    feat: add id to getUserInfo and setUserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,23 +1,25 @@
 class UserInfo {
-    constructor({ nameSelector, titleSelector, avatarSelector }) {
-        this._nameElement = document.querySelector(nameSelector),
-            this._titleElement = document.querySelector(titleSelector),
-            this._avatarElement = document.querySelector(avatarSelector)
-    }
+  constructor({ nameSelector, titleSelector, avatarSelector} ) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._titleElement = document.querySelector(titleSelector);
+    this._avatarElement = document.querySelector(avatarSelector);
+  }
 
-    getUserInfo() {
-        return { 
-            name: this._nameElement.textContent, 
-            about: this._titleElement.textContent, 
-            avatar: this._avatarElement.src
-         }
-    }
+  getUserInfo() {
+    return {
+      name: this._nameElement.textContent,
+      about: this._titleElement.textContent,
+      avatar: this._avatarElement.src,
+      id: this._id,
+    };
+  }
 
-    setUserInfo(res) {
-        this._nameElement.textContent = res.name
-        this._titleElement.textContent = res.about
-        this._avatarElement.src = res.avatar
-    }
+  setUserInfo(res) {
+    this._nameElement.textContent = res.name;
+    this._titleElement.textContent = res.about;
+    this._avatarElement.src = res.avatar;
+    this._id = res._id;
+  }
 }
 
-export default UserInfo
+export default UserInfo;
